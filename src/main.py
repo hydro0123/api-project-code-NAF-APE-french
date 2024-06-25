9ce671dd05022d0abdd5205fcfbed76eb38bfc24
hydro0123 <gabrielhydro@gmail.com>
2024-06-25 10:48:30 +0200

    added documentation to the API

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
 import uvicorn
 from colorama import init
 import pandas as pd
@@ -27,11 +27,20 @@ def Code_APE_Label(Code,NIV):
     return(code_ape_get_label(Code,NIV,table_CodeAPE))
 
 @app.get("/R11")
-def Ratio_11_solvabilite(Code,TB,FP):
+def Ratio_11_solvabilite(Code=Query(...,description="Code APE de l'entreprise."),
+                         TB=Query(...,description="Total Bilan à insérer, code EE sur liasse fiscale en 2050."),
+                         FP=Query(...,description="Fonds Propres à insérer, code DL sur liasse fiscale en 2050.")):
     return(BACH_R11(Code,TB,FP,table_BACH))
 
 @app.get("/R28")
-def Ratio_28(Code,DU,YQ,YR,YS,CF,CG,EE):
+def Ratio_28(Code=Query(...,description="Code APE de l'entreprise."),
+             DU=Query(...,description="emprunts et dettes auprès desetablissements de crédit, code DU sur liasse fiscale en 2050."),
+             YQ=Query(...,description="engagement de crédit-bail mobilier, code YQ sur liasse fiscale en 2050."),
+             YR=Query(...,description="engagement de crédit-bail immobilier, code YR sur liasse fiscale en 2050."),
+             YS=Query(...,description="effets portés a l'escompte et non échus, code YS sur liasse fiscale en 2050."),
+             CF=Query(...,description="disponibilités, code CF sur liasse fiscale en 2050."),
+             CG=Query(...,description="disponibilités, code CG sur liasse fiscale en 2050."),
+             EE=Query(...,description="Total Bilan, code EE sur liasse fiscale en 2050.")):
     return(BACH_R28(Code,DU,YQ,YR,YS,CF,CG,EE,table_BACH))
 
 if __name__ == "__main__":
