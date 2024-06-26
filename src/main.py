2dd895b1318aa0b107f0d64b6b52aa23a36c19b0
hydro0123 <gabrielhydro@gmail.com>
2024-06-26 16:40:01 +0200

    removed en in laisse fiscale en 2050

--- src/main.py
+++ src/main.py
@@ -28,19 +28,19 @@ def Code_APE_Label(Code,NIV):
 
 @app.get("/R11")
 def Ratio_11_solvabilite(Code=Query(...,description="Code APE de l'entreprise."),
-                         TB=Query(...,description="Total Bilan à insérer, code EE sur liasse fiscale en 2050."),
-                         FP=Query(...,description="Fonds Propres à insérer, code DL sur liasse fiscale en 2050.")):
+                         TB=Query(...,description="Total Bilan à insérer, code EE sur liasse fiscale  2050."),
+                         FP=Query(...,description="Fonds Propres à insérer, code DL sur liasse fiscale 2050.")):
     return(BACH_R11(Code,TB,FP,table_BACH))
 
 @app.get("/R28")
 def Ratio_28(Code=Query(...,description="Code APE de l'entreprise."),
-             DU=Query(...,description="emprunts et dettes auprès desetablissements de crédit, code DU sur liasse fiscale en 2050."),
-             YQ=Query(...,description="engagement de crédit-bail mobilier, code YQ sur liasse fiscale en 2050."),
-             YR=Query(...,description="engagement de crédit-bail immobilier, code YR sur liasse fiscale en 2050."),
-             YS=Query(...,description="effets portés a l'escompte et non échus, code YS sur liasse fiscale en 2050."),
-             CF=Query(...,description="disponibilités, code CF sur liasse fiscale en 2050."),
-             CG=Query(...,description="disponibilités, code CG sur liasse fiscale en 2050."),
-             EE=Query(...,description="Total Bilan, code EE sur liasse fiscale en 2050.")):
+             DU=Query(...,description="emprunts et dettes auprès desetablissements de crédit, code DU sur liasse fiscale 2050."),
+             YQ=Query(...,description="engagement de crédit-bail mobilier, code YQ sur liasse fiscale 2050."),
+             YR=Query(...,description="engagement de crédit-bail immobilier, code YR sur liasse fiscale 2050."),
+             YS=Query(...,description="effets portés a l'escompte et non échus, code YS sur liasse fiscale 2050."),
+             CF=Query(...,description="disponibilités, code CF sur liasse fiscale 2050."),
+             CG=Query(...,description="disponibilités, code CG sur liasse fiscale 2050."),
+             EE=Query(...,description="Total Bilan, code EE sur liasse fiscale 2050.")):
     return(BACH_R28(Code,DU,YQ,YR,YS,CF,CG,EE,table_BACH))
 
 if __name__ == "__main__":
